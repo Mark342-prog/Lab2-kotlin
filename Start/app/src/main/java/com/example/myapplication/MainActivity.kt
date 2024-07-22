f823937b6d9ee9d9bd3497740e45a82b49a7ac89
Mark342-prog <84196496+Mark342-prog@users.noreply.github.com>
2024-07-22 06:52:00 -0600

    update semi-final

--- Start/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Start/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.renderscript.Int2
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,36 +14,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
-
+fun promedio(numbers: List<Int>): Double {
+    var sum = 0.0
+    for (num in numbers) {
+        sum += num
+    }
+    val average = sum / numbers.size
+    return average
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        val num = listOf(10, 2, 3, 40, 5)
+        Log.i("List test", promedio(num).toString())
+        val numbersArray = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        val oddArray = numbersArray.filter{ it % 2 != 0 }
+        Log.i("List test",oddArray.toString())
+        fun isPalindrome(x: Int): Boolean {
+            val original = x.toString()
+            val rever = original.reversed()
+            return original == rever
+        }
+        Log.i("Palidrome", isPalindrome(12321).toString())
+        fun performOperation(int: Int, int2: Int, op:String): Int {
+            when (op) {
+                "+" -> return (int + int2)
+                "-" -> return  (int - int2)
+                else -> {return 0}
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+        Log.i("operation", performOperation(5,5,"+").toString())
     }
 }
